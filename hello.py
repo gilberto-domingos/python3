9554ec92778bfa28c5c7ada62beab0339bc7dffa
gilberto-domingos <domingoshot@hotmail.com>
2024-05-02 11:09:11 -0300

    test commit

--- hello.py
+++ hello.py
@@ -2,7 +2,7 @@
 The application is defined using the Flask class from the flask module.
 The route("/") function is a decorator that maps the root URL to the hello function.
 The hello function returns a string containing the text "Hello World!".
-The if __name__ == "__main__": block starts the Flask application when the file is executed.
+The if __name__ ==ss "__main__": block starts the Flask application when the file is executed.
 """
 from flask import Flask
 
