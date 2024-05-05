910666f7eeb197d87347545f2d4a80f6486e03dd
gilberto-domingos <domingoshot@hotmail.com>
2024-05-05 17:05:17 -0300

    aaabbb

--- pre-commit.py
+++ pre-commit.py
@@ -3,13 +3,19 @@
 # Import libraries
 import os
 
-# Define the message to save
-message = "TESTE , SALVANDO NO ARQUIVO COMMIT_EDITMSG"
+# Define the message to save and print
+message = "TESTE, SALVANDO NO ARQUIVO COMMIT_EDITMSG"
+
 
 def pre_commit():
-  """Saves the message to the COMMIT_EDITMSG file."""
-  with open(os.path.join(".git", "COMMIT_EDITMSG"), "w") as commit_file:
-    commit_file.write(message)
+    """Saves the message to the COMMIT_EDITMSG file and prints it."""
+    # Salva a mensagem no arquivo COMMIT_EDITMSG
+    with open(os.path.join(".git", "COMMIT_EDITMSG"), "w") as commit_file:
+        commit_file.write(message)
+
+    # Imprime a mensagem no terminal
+    print("Mensagem do commit:", message)
+
 
 if __name__ == "__main__":
-  pre_commit()
+    pre_commit()
