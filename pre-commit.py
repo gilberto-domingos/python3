137d46aa6a2974c7190f1a9ea2e087f2f8901fe3
gilberto-domingos <domingoshot@hotmail.com>
2024-05-06 23:03:22 -0300

    new pre-commit-o

---
+++ pre-commit.py
@@ -0,0 +1,7 @@
+import sys
+
+def run_hook():
+    print("FUNCIONANDO O PRE-COMMIT HOOK")
+
+if __name__ == "__main__":
+    run_hook()
--- test.py
+++ test.py
@@ -1,3 +1,3 @@
 # hello asdf
-NAME = "gilbssSDsssdsdssd"
+NAME = "gilbssSDsssssssdsdsssd"
 print(NAME)
