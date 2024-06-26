3afb5390af1c811612a446e436575763f538df7c
gilberto-domingos <domingoshot@hotmail.com>
2024-06-26 15:59:00 -0300

    django (#10862)

---
+++ django_site/home/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+
+# Create your models here.
+class position(models.Model):
+    position_name = models.CharField(max_length=200, blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    edited_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.position_name
+
+
+class Employee(models.Model):
+    emp_name = models.CharField(max_length=50, null=False, blank=False)
+    emp_salary = models.IntegerField(null=False, blank=False)
