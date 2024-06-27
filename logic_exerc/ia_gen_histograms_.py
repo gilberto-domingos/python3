c46b4f4e7219c50a656597c3aeef08b5b6e2c41d
gilberto-domingos <domingoshot@hotmail.com>
2024-06-27 14:34:39 -0300

    tensor flow (#10866)

--- logic_exerc/ia_gen_histograms_.py
+++ logic_exerc/ia_gen_histograms_.py
@@ -1,5 +1,22 @@
 import matplotlib.pyplot as plt
 import numpy as np
+
+t = np.arange(0.01, 5.0, 0.01)
+
+s = np.exp(-t)
+fig, ax = plt.subplots()
+
+ax.plot(t, s)
+ax.set_xlabel('Tempo')
+ax.set_ylabel('Quantidade')
+ax.set_title('Exponencial Decrescente')
+ax.grid(True)
+
+plt.show()
+
+'''
+import matplotlib.pyplot as plt
+import numpy as np
 from numpy.random._examples.cffi.extending import rng
 
 N_points = 100000
@@ -16,3 +33,4 @@ ax.set_title('Histograma')
 
 
 plt.show()
+'''
---
+++ logic_exerc/ia_tensor_flow.py
@@ -0,0 +1,3 @@
+import tensor_flow as tf
+
+print(tf.reduce_sum(tf.random.normal([1000, 1000])))
