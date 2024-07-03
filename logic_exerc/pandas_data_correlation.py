9ffc41d65d8a55cf5e16b10c6efc16a87cec92fd
gilberto-domingos <domingoshot@hotmail.com>
2024-07-03 14:44:34 -0300

    pandas_data_correlation (#94832)

--- logic_exerc/pandas_data_correlation.py
+++ logic_exerc/pandas_data_correlation.py
@@ -2,4 +2,14 @@ import pandas as pd
 
 df = pd.read_csv("files/sport-activity3.csv")
 
-print(df)
+pd.set_option('display.max_rows', 169)
+
+# print(df)
+
+# rows_na = [df['Calories'].isna()]
+
+# nan_calories = df[df['Calories'].isna()]
+
+# print(nan_calories)
+
+print(df.corr())
