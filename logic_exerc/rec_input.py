9ff073f87e5617818639ddffae686168d6318972
gilberto-domingos <domingoshot@hotmail.com>
2024-07-05 14:52:31 -0300

    exer rec_input (#94836)

---
+++ logic_exerc/rec_input.py
@@ -0,0 +1,10 @@
+# Recebendo os valores inteiros A e B como entrada
+A = int(input("Digite o valor de A: "))
+B = int(input("Digite o valor de B: "))
+
+# Verificando se os valores são iguais
+if A == B:
+    print(f"O valor {A} é igual a {B}")
+else:
+    print(f"O valor {A} é diferente de {B}")
+10
