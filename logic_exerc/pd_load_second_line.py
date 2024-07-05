3685067b8a2fb7223f7b64fe770c55572965cf02
gilberto-domingos <domingoshot@hotmail.com>
2024-07-05 14:41:05 -0300

    exer pd_load_second_line.py (#94834)

---
+++ logic_exerc/pd_load_second_line.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+pedido = {
+      'idCliente': [2995, 3001, 1112, 9999],
+      'produto': ['Refrigerador 220V', 'Smartphone 10', 'Monitor 19 LCD', 'Mouse Logitech'],
+      'valor': [2649.99, 4999.00, 679.90, 29.90]
+}
+
+# Carrega o Set em um dataframe df
+df = pd.DataFrame(pedido)
+
+# Carregar a segunda linha do dataframe
+print(df.loc[1])
