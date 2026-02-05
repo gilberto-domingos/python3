0444c8679f49c8eb4e1b090c2166e3e76feaa97c
gilberto-domingos <gilberto1domingos1@outlook.com>
2026-02-04 22:45:29 -0300

    feat: primitive types

---
+++ logic_exerc/primitive_types.py
@@ -0,0 +1,5 @@
+n1 = int(input('Digite um numero: '))
+n2 = int(input('Digite outro numero: '))
+sum = n1 + n2
+print('O total é {}'.format(sum))
+print('As soma entre {} e {} vale {}'.format(n1, n2, sum))
